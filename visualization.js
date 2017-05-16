7a5a99cdd89d14426c50643df7258d984b6e9f83
Sang Theresa Lee <tsahlee@gmail.com>
2017-05-16 00:28:50 -0400

    Draw choropleth

--- visualization.js
+++ visualization.js
@@ -13,7 +13,15 @@
     d3.queue()
         .defer(d3.json, "us.topo.json")
         .defer(d3.json, "usa_zip_map.json")
+        .defer(d3.csv, "college_data.csv")
         .await(ready)
+
+var color_domain = [50, 150, 350, 750, 1500]
+  var ext_color_domain = [0, 50, 150, 350, 750, 1500]
+  var legend_labels = ["< 50", "50+", "150+", "350+", "750+", "> 1500"]
+  var color = d3.scaleThreshold()
+  .domain(color_domain)
+  .range(["#adfcad", "#ffcb40", "#ffba00", "#ff7d73", "#ff4e40", "#ff1300"]);
 
 
     // projection is for taking something like the world, which is in a sphere, and fitting it intp a flat screen
@@ -25,17 +33,29 @@
     var path = d3.geoPath()
                 .projection(projection)
 
-    function ready (error, data, zips) {
-        // console.log(zips)
+    function ready (error, data, zips, college_data) {
+
+        var popById = {}
+        var zipById = {}
+
+        college_data.forEach(function(d) {
+            popById[d.zipcode]= +d.size
+            // zipById[id] = d.zipcode
+        })
 
         var zipcodes = topojson.feature(zips, zips.objects.zip_codes_for_the_usa).features
+        console.log(college_data)
         console.log(zipcodes)
 
-        svg.selectAll(".county")
+        svg.selectAll(".zipcode")
         .data(zipcodes)
         .enter().append("path")
-        .attr("class", "county")
+        .attr("class", "zipcode")
         .attr("d", path)
+        .style("fill", function(d) {
+            return color(popById[d.properties.zip]);
+          })
+          .style("opacity", 0.8)
 
 
         // svg.selectAll(".county")
@@ -48,7 +68,6 @@
         Always pass the data, then data.objectes.__something__ then get .features out of it
         */
         var states = topojson.feature(data, data.objects.state).features
-        console.log(states)
 
         // add paths for each state
         svg.selectAll(".state")
@@ -56,6 +75,25 @@
             .enter().append("path")
             .attr("class", "state")
             .attr('d', path)
+
+        // svg.selectAll('.college')
+        //     .data(college_data)
+        //     .enter().append("circle")
+        //     .attr('class', 'college')
+        //     .attr("r", 2)
+        //     .attr("cx", function (d) {
+        //         var coords = projection([d.long, d.lat])
+        //         if (coords) {
+        //             return coords[0]
+        //         }
+        //     })
+        //     .attr("cy", function (d) {
+        //         var coords = projection([d.long, d.lat])
+        //         if (coords) {
+        //            return coords[1]
+        //     }
+        //     })
+
     }
 
 })();
